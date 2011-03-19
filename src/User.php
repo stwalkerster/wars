950710433c49d4a3ce692528b35c36276636f179
stwalkerster <stwalkerster@ff9924c4-b166-4daa-9a09-dcd427f38e35>
2011-03-19 02:39:26 +0000

    update User class to use cool PDO stuff

--- src/User.php
+++ src/User.php
@@ -25,20 +25,15 @@ class User implements DataObject
 {
 	public static function authenticate($username, $password)
 	{
-		$select = new DatabaseSelect();
-		$select->fields = array('user_id');
-		$select->from = 'user';
-		$select->addWhere('user_pass', '=', User::saltPassword($username , $password));
-		$select->addWhere('user_name', '=', $username );
-		
 		global $accDatabase;
-		$resultset = $accDatabase->getResultSet($select,MYSQL_ASSOC);
-		
-		if(isset($resultset[0]))
+		$statement = $accDatabase->prepare("SELECT * FROM user WHERE user_name = :username AND user_password = :password LIMIT 1;");
+		$statement->bindParam(":username", $username);
+		$statement->bindParam(":password", $password);
+		$resultUser = $statement->fetchObject("User");
+	
+		if($resultUser != false)
 		{
-			$u = User::getById($resultset[0]['user_id']);
-			$_SESSION['currentUser'] = serialize($u);
-			
+			$_SESSION['currentUser'] = serialize($resultUser);
 			return true;
 		}
 		else
@@ -48,67 +43,17 @@ class User implements DataObject
 
 	public static function getByName($username)
 	{		
-		$select = new DatabaseSelect();
-		$select->fields = array('*');
-		$select->from = 'user';
-		$select->addWhere('user_name', '=', $username );
-		$select->limit=1;
-		
 		global $accDatabase;
-		$resultset = $accDatabase->getResultSet($select,MYSQL_ASSOC);
-		
-		$c = new User();
-		
-		$c->id = $resultset[0]['user_id'];
-		$c->name = $username;
-		$c->email = $resultset[0]['user_email'];
-		$c->pass = $resultset[0]['user_pass'];
-		$c->admin = $resultset[0]['user_admin'];
-		$c->developer = $resultset[0]['user_developer'];
-		$c->suspended = $resultset[0]['user_suspended'];
-		$c->new = $resultset[0]['user_new'];
-		$c->viewprivate = $resultset[0]['user_viewprivate'];
-		$c->onwiki = $resultset[0]['user_onwiki'];
-		$c->welcome = $resultset[0]['user_welcome'];
-		$c->welcometemplate = $resultset[0]['user_welcometemplate'];
-		$c->secureserver = $resultset[0]['user_secureserver'];
-		$c->signature = $resultset[0]['user_signature'];
-		
-		$c->newRecord = 0;
-		
-		return $c;
+		$statement = $accDatabase->prepare("SELECT * FROM user WHERE user_name = :username LIMIT 1;");
+		$statement->bindParam(":username",$username);
+		return $statement->fetchObject("User");
 	}
 	public static function getById($id)
 	{
-		$select = new DatabaseSelect();
-		$select->fields = array('*');
-		$select->from = 'user';
-		$select->addWhere('user_id', '=', $id );
-		$select->limit=1;
-		
 		global $accDatabase;
-		$resultset = $accDatabase->getResultSet($select,MYSQL_ASSOC);
-		
-		$c = new User();
-		
-		$c->id = $id;
-		$c->name = $resultset[0]['user_name'];
-		$c->email = $resultset[0]['user_email'];
-		$c->pass = $resultset[0]['user_pass'];
-		$c->admin = $resultset[0]['user_admin'];
-		$c->developer = $resultset[0]['user_developer'];
-		$c->suspended = $resultset[0]['user_suspended'];
-		$c->new = $resultset[0]['user_new'];
-		$c->viewprivate = $resultset[0]['user_viewprivate'];
-		$c->onwiki = $resultset[0]['user_onwiki'];
-		$c->welcome = $resultset[0]['user_welcome'];
-		$c->welcometemplate = $resultset[0]['user_welcometemplate'];
-		$c->secureserver = $resultset[0]['user_secureserver'];
-		$c->signature = $resultset[0]['user_signature'];
-		
-		$c->newRecord = 0;
-		
-		return $c;	
+		$statement = $accDatabase->prepare("SELECT * FROM user WHERE user_id = :userid LIMIT 1;");
+		$statement->bindParam(":userid",$id);
+		return $statement->fetchObject("User");
 	}
 	
 	public static function saltPassword($username, $password)
@@ -118,60 +63,62 @@ class User implements DataObject
 	
 	public function __construct($username, $password, $email, $onwiki)
 	{
-		$this->id = 0;
-		$this->name = $username;
-		$this->email = $email;
-		$this->pass = User::saltPassword($username, $password);
-		$this->admin = 0;
-		$this->developer = 0;
-		$this->suspended = 0;
-		$this->new = 1;
-		$this->viewprivate = 0;
-		$this->onwiki = $onwiki;
-		$this->welcome = 0;
-		$this->welcometemplate = 1;
-		$this->secureserver = 0;
-		$this->signature = "";
+		$this->user_id = 0;
+		$this->user_name = $username;
+		$this->user_email = $email;
+		$this->user_pass = User::saltPassword($username, $password);
+		$this->user_level = "New";
+		$this->user_onwikiname = $onwiki;
+		$this->user_welcome_sig = "";
+		$this->user_welcome_templateid = 0;
+		$this->user_lastactive = date("Y-m-d H:i:s", 0);
+		$this->user_lastip = "0.0.0.0";
+		$this->user_forcelogout = 0;
+		$this->user_secure = 0;
+		$this->user_checkuser = 0;
+		$this->user_identified = 0;
+		$this->user_abortpref = 0;
+		$this->user_confirmationdiff = 0;
 		
 		$this->newRecord = 1;
 	}
 	
-	private $id, $name, $email, $pass, $admin, $developer, $suspended, $new;
-	private $viewprivate, $onwiki, $welcome, $welcometemplate, $secureserver;
-	private $signature;
+	private $user_id, $user_name, $user_email, $user_pass, $user_level, $user_onwikiname, $user_welcome_sig, 
+		$user_welcome_templateid, $user_lastactive, $user_lastip, $user_forcelogout, $user_secure, 
+		$user_checkuser, $user_identified, $user_abortpref, $user_confirmationdiff;
 	
 	private $newRecord;
 	
 	public function getId()
 	{ 
-		return $this->id; 
+		return $this->user_id; 
 	}
 	
 	public function getUsername()
 	{ 
-		return $this->name; 
+		return $this->user_name; 
 	}
 	
 	public function getEmail() // looking into privacy at the moment
 	{
-		return $this->email;
+		return $this->user_email;
 	} 
 	public function setEmail($newemail) // security, is current user an admin, or is this the current user?
 	{
-		$this->email = $newemail;
+		$this->user_email = $newemail;
 	} 
 	
 	public function setPass($newpass, $oldpass)
 	{
 		if(User::saltPassword($this->username, $oldpass) == $this->pass)
 		{
-			$this->pass = User::saltPassword($this->username, $newpass);
+			$this->user_pass = User::saltPassword($this->username, $newpass);
 		}
 	}
 	
 	public function isAdmin() 
 	{
-		return $this->admin;
+		return $this->user_level == "Admin";
 	}
 	public function promoteAdmin( ) // security, is current user an admin?
 	{
@@ -184,12 +131,12 @@ class User implements DataObject
 	
 	public function getDeveloper() 
 	{
-		return $this->developer;
+		trigger_error("Not implemented.");
 	}
 	
 	public function isSuspended() 
 	{
-		return $this->suspended;
+		return $this->user_level == "Suspended";
 	}
 	public function suspend( $reason ) // security, is current user an admin?
 	{
@@ -202,7 +149,7 @@ class User implements DataObject
 	
 	public function isNew() 
 	{
-		return $this->new;
+		return $this->user_level == "New";
 	}
 	public function approve( ) // security, is current user an admin?
 	{
@@ -211,52 +158,43 @@ class User implements DataObject
 	
 	public function isAllowedPrivateData()
 	{
-		return $this->viewprivate;
+		trigger_error("Not implemented.");
 	}
 	
 	public function getOnwikiName()
 	{
-		return $this->onwiki;
+		return $this->user_onwikiname;
 	} 
 	public function setOnwikiName($username) // security, is current user an admin?
 	{
 		trigger_error("Not implemented.");
 	}
 	
-	public function getWelcomeStatus()
-	{
-		return $this->welcome;
-	}
-	public function setWelcomeStatus($welcomeUsers)
-	{
-		$this->welcome = $welcomeUsers ;	
-	}
-	
 	public function getTemplate()
 	{
-		return $this->welcomeTemplate;
+		return $this->user_welcome_templateid;
 	}
 	public function setTemplate($id)
 	{
-		$this->welcometemplate = $id;
+		$this->user_welcome_templateid = $id;
 	}
 	
 	public function getSecureStatus()
 	{
-		return $this->secureserver;
+		return $this->user_secure;
 	}
 	public function setSecureStatus($useSecureServer)
 	{
-		$this->secureserver = $useSecureServer;
+		$this->user_secure = $useSecureServer;
 	}
 	
 	public function getSignature()
 	{
-		return $this->signature;
+		return $this->user_welcome_sig;
 	}
 	public function setSignature($signature)
 	{
-		$this->signature=$signature;
+		$this->user_welcome_sig=$signature;
 	}
 	
 	public function save()
@@ -264,27 +202,12 @@ class User implements DataObject
 		global $accDatabase;
 		if($this->newRecord)
 		{ // INSERT
-//			$values = array(
-//					$this->name, $this->email, $this->pass, $this->admin, $this->developer, 
-//					$this->suspended, $this->new, $this->viewprivate, $this->onwiki, $this->welcome, 
-//					$this->welcometemplate, $this->secureserver, $this->signature
-//					);
-//			
-//			$this->id = $accDatabase->insert('user', $values);
 			
 			$this->newRecord = 0;
 		}
 		else
 		{ // UPDATE
-//			$values = array(
-//					'user_email' => $this->email, 'user_pass' => $this->pass, 'user_admin' => $this->admin, 
-//					'user_developer' => $this->developer, 'user_suspended' => $this->suspended, 
-//					'user_new' => $this->new, 'user_onwiki' => $this->onwiki, 
-//					'user_welcome' => $this->welcome, 'user_welcometemplate' => $this->welcometemplate, 
-//					'user_secureserver' => $this->secureserver, 'user_signature' => $this->signature
-//					);
-//			
-//			$accDatabase->update('user', $values, "user_id = $this->id", 1);
+
 		}
 	}
 }

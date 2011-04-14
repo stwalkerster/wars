89600a9289fb450acfd1b1bceddc3b090d6dfebb
Simon Walker <github@helpmebot.org.uk>
2011-04-14 07:59:37 +0100

    add the redirect to point users to the new URL

---
+++ acc.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Hey!</title>
+</head>
+<body>
+<h1>Hey! You! Yes, YOU!</h1>
+<p>Update your bookmarks! The interface is now at <a href="internal.php">internal.php</a>, as we're now using the all-new Wikipedia Account Request System rewrite!</p>
+<p>Now, go fix your links, and go to the right place next time!</p>
+</body>
+</html>

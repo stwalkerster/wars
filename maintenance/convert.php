c3f0b43a1ec370dc50d1b7fcf01b5e32f59feac4
Simon Walker <github@helpmebot.org.uk>
2011-04-15 07:29:52 +0100

    fix?

--- maintenance/convert.php
+++ maintenance/convert.php
@@ -30,6 +30,12 @@ foreach ($tables_sql as $key => $value)
 {
 	out("  - $key");
 	$result = $accDatabase->query($value);
+	if(get_class($result) != "PDOStatement")
+	{
+		out("Error! Could not retrieve result!");
+		echo $value;
+		die();
+	}
 	$error = $result->errorInfo();
 	if($error[0] != "0000")
 	{
@@ -50,19 +56,67 @@ foreach ($tables_sql as $key => $value)
 out("Stage two: Add existing simple data");
 
 out("  - acc_user");
-$accDatabase->query("INSERT INTO acc_user SELECT user_id, user_name, user_email, user_pass, user_level, user_onwikiname, user_welcome_sig, user_lastactive, user_lastip, user_forcelogout, user_secure, user_checkuser, user_identified, user_welcome_templateid, user_abortpref, user_confirmationdiff FROM $olddatabase.acc_user; ");
+$result = $accDatabase->query("INSERT INTO acc_user SELECT user_id, user_name, user_email, user_pass, user_level, user_onwikiname, user_welcome_sig, user_lastactive, user_lastip, user_forcelogout, user_secure, user_checkuser, user_identified, user_welcome_templateid, user_abortpref, user_confirmationdiff FROM $olddatabase.acc_user; ");
+if(get_class($result) != "PDOStatement")
+{
+	out("Error! Could not retrieve result!");
+	echo $value;
+	die();
+}
+$error = $result->errorInfo();
+if($error[0] != "0000")
+{
+	print_r($error);
+	die;
+}
 out("    Done!");
 
 out("  - acc_pend");
-$accDatabase->query("INSERT INTO acc_pend SELECT * FROM $olddatabase.acc_pend;");
+$result = $accDatabase->query("INSERT INTO acc_pend SELECT * FROM $olddatabase.acc_pend;");
+if(get_class($result) != "PDOStatement")
+{
+	out("Error! Could not retrieve result!");
+	echo $value;
+	die();
+}
+$error = $result->errorInfo();
+if($error[0] != "0000")
+{
+	print_r($error);
+	die;
+}
 out("    Done!");
 
 out("  - acc_template");
-$accDatabase->query("INSERT INTO acc_template SELECT * FROM $olddatabase.acc_template;");
+$result = $accDatabase->query("INSERT INTO acc_template SELECT * FROM $olddatabase.acc_template;");
+if(get_class($result) != "PDOStatement")
+{
+	out("Error! Could not retrieve result!");
+	echo $value;
+	die();
+}
+$error = $result->errorInfo();
+if($error[0] != "0000")
+{
+	print_r($error);
+	die;
+}
 out("    Done!");
 
 out("  - acc_emails");
-$accDatabase->query("INSERT INTO acc_emails SELECT * FROM $olddatabase.acc_emails;");
+$result = $accDatabase->query("INSERT INTO acc_emails SELECT * FROM $olddatabase.acc_emails;");
+if(get_class($result) != "PDOStatement")
+{
+	out("Error! Could not retrieve result!");
+	echo $value;
+	die();
+}
+$error = $result->errorInfo();
+if($error[0] != "0000")
+{
+	print_r($error);
+	die;
+}
 out("    Done!");
 
 /*

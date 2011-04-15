b1d8a646f14be6b960d40f47d568c39eb67c9ee9
Simon Walker <github@helpmebot.org.uk>
2011-04-15 08:41:44 +0100

    port RecreateTrustedIPs script

---
+++ maintenance/RecreateTrustedIPs.php
@@ -0,0 +1,52 @@
+<?php
+if (isset($_SERVER['REQUEST_METHOD'])) {
+	die();
+} // Web clients die.
+define("WARS",1);
+require_once 'config.inc.php';
+
+$htmlfile = file_get_contents('http://www.wikimedia.org/trusted-xff.html');
+$matchfound = preg_match_all('/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/', $htmlfile, $matches, PREG_SET_ORDER);
+if (!$matchfound)
+	die('ERROR: No IPs found on trusted XFF page.');
+
+$ip=array();
+	
+foreach ($matches as $match) {
+	$ip[] = $match[0];
+}
+
+$ip=array_unique($ip);
+
+$sqlquery = 'INSERT INTO `acc_trustedips` (`trustedips_ipaddr`) VALUES ';
+foreach ($ip as $i) {
+	$sqlquery .= "('$i'), ";
+}
+$sqlquery = substr($sqlquery, 0, -2) . ';';
+
+mysql_connect($toolserver_host, $toolserver_username, $toolserver_password);
+@ mysql_select_db($toolserver_database) or die(mysql_error());
+
+if($accDatabase->beginTransaction())
+{
+	
+	$success1 = $accDatabase->exec("DELETE FROM `acc_trustedips`;");
+	if($success1 === FALSE )
+	{
+		print_r($accDatabase->errorInfo());
+		$accDatabase->rollBack();
+		die;
+	}
+
+	$success2 = $accDatabase->exec($sqlquery);
+	if($success2 === FALSE )
+	{
+		print_r($accDatabase->errorInfo());
+		$accDatabase->rollBack();
+		die;
+	}
+
+	$accDatabase->commit();
+}
+else
+	echo "Error starting transaction.\n";

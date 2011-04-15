d3cab4987b27e3e2f879ab316d6e082e3cc2f619
Simon Walker <github@helpmebot.org.uk>
2011-04-15 08:08:49 +0100

    fix dependencies, tweak table

--- maintenance/convert.php
+++ maintenance/convert.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * Stage zero: Set up the environment
+ */
+
 define("WARS", 1);
 require_once 'config.inc.php';
 
@@ -13,12 +17,6 @@ for ($i = 10; $i > 0; $i--) {
 	out($i);
 	sleep(1);
 }
-
-/*
- * Stage zero: Set up the environment
- */
-
-
 
 /*
  * Stage one: Create database tables
@@ -47,45 +45,11 @@ foreach ($tables_sql as $key => $value)
 
 /*
  * Stage two: Add existing simple data:
- *  * user
- *  * pend
  *  * template
  *  * emails
  */
  
 out("Stage two: Add existing simple data");
-
-out("  - acc_user");
-$result = $accDatabase->query("INSERT INTO acc_user SELECT user_id, user_name, user_email, user_pass, user_level, user_onwikiname, user_welcome_sig, user_lastactive, user_lastip, user_forcelogout, user_secure, user_checkuser, user_identified, user_welcome_templateid, user_abortpref, user_confirmationdiff FROM $olddatabase.acc_user; ");
-if(get_class($result) != "PDOStatement")
-{
-	out("Error! Could not retrieve result!");
-	echo $value;
-	die();
-}
-$error = $result->errorInfo();
-if($error[0] != "0000")
-{
-	print_r($error);
-	die;
-}
-out("    Done!");
-
-out("  - acc_pend");
-$result = $accDatabase->query("INSERT INTO acc_pend SELECT * FROM $olddatabase.acc_pend;");
-if(get_class($result) != "PDOStatement")
-{
-	out("Error! Could not retrieve result!");
-	echo $value;
-	die();
-}
-$error = $result->errorInfo();
-if($error[0] != "0000")
-{
-	print_r($error);
-	die;
-}
-out("    Done!");
 
 out("  - acc_template");
 $result = $accDatabase->query("INSERT INTO acc_template SELECT * FROM $olddatabase.acc_template;");
@@ -104,7 +68,32 @@ if($error[0] != "0000")
 out("    Done!");
 
 out("  - acc_emails");
-$result = $accDatabase->query("INSERT INTO acc_emails SELECT * FROM $olddatabase.acc_emails;");
+$result = $accDatabase->query("INSERT INTO acc_emails SELECT mail_id, mail_text, mail_count, mail_desc, mail_type, substr(mail_desc,1,45) FROM $olddatabase.acc_emails;");
+if(get_class($result) != "PDOStatement")
+{
+	out("Error! Could not retrieve result!");
+	echo $value;
+	die();
+}
+$error = $result->errorInfo();
+if($error[0] != "0000")
+{
+	print_r($error);
+	die;
+}
+out("    Done!");
+
+
+
+/*
+ * Stage three: Add dependant data:
+ *  * user
+ */
+ 
+out("Stage three: Add dependant data");
+
+out("  - acc_user");
+$result = $accDatabase->query("INSERT INTO acc_user SELECT user_id, user_name, user_email, user_pass, user_level, user_onwikiname, user_welcome_sig, user_lastactive, user_lastip, user_forcelogout, user_secure, user_checkuser, user_identified, user_welcome_templateid, user_abortpref, user_confirmationdiff FROM $olddatabase.acc_user; ");
 if(get_class($result) != "PDOStatement")
 {
 	out("Error! Could not retrieve result!");
@@ -120,19 +109,44 @@ if($error[0] != "0000")
 out("    Done!");
 
 /*
- * Stage three: Add partially calculated dependant data:
+ * Stage four: Add dependant data:
+ *  * pend
+ */
+
+out("Stage four: Add dependant data");
+
+out("  - acc_pend");
+$result = $accDatabase->query("INSERT INTO acc_pend SELECT * FROM $olddatabase.acc_pend;");
+if(get_class($result) != "PDOStatement")
+{
+	out("Error! Could not retrieve result!");
+	echo $value;
+	die();
+}
+$error = $result->errorInfo();
+if($error[0] != "0000")
+{
+	print_r($error);
+	die;
+}
+out("    Done!");
+
+/*
+ * Stage five: Add partially calculated dependant data:
  *  * welcome
- *  * log
  *  * ban
+ *  * log
  *  * cmt
  */
- 
+out("Stage five: Add partially calculated dependant data");
+
 /*
- * Stage four: Add fully calculated data (run maintenance scripts)
+ * Stage six: Add fully calculated data (run maintenance scripts)
  *  * trusted ips
  *  * titleblacklist
  */
+out("Stage six: Add fully calculated data (run maintenance scripts)");
  
 /*
- * Stage five: Add indices, foreign keys, and other assorted produce
+ * Stage seven: Add indices, foreign keys, and other assorted produce
  */

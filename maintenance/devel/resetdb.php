2b950e08386065f9f1915cfdac252dd9401e6769
Simon Walker <github@helpmebot.org.uk>
2011-04-16 17:59:39 +0100

    fix up minor bugs

--- maintenance/devel/resetdb.php
+++ maintenance/devel/resetdb.php
@@ -9,4 +9,4 @@ require_once $baseMaintenancePath . 'convert.inc';
 query("DROP TABLE $olddatabase.acc_log;");
 query("CREATE TABLE $olddatabase.acc_log LIKE $olddatabase.log_bu;");
 query("INSERT INTO $olddatabase.acc_log SELECT * FROM $olddatabase.log_bu;");
-out("done\n");
+out("done");

ad90bb7d54e01601a027fc6c2a209f129a83e132
Simon Walker <github@helpmebot.org.uk>
2011-04-16 11:19:42 +0100

    add classes for new data objects

---
+++ src/TitleBlacklist.php
@@ -0,0 +1,27 @@
+<?php
+/**************************************************************************
+*                     Wikipedia Account Request System                    *
+***************************************************************************
+*                                                                         *
+* Conceptualised by Incubez (author: X!) and ACC (author: SQL and others) *
+*                                                                         *
+* Please refer to /LICENCE for more info.                                 *
+*                                                                         *
+**************************************************************************/
+
+// check that this code is being called from a valid entry point. 
+if(!defined("WARS"))
+	die("Invalid code entry point!");
+	
+class TitleBlacklist extends DataObject
+{	
+	public function save()
+	{
+		trigger_error("Not implemented");
+	}
+	
+	public function getMessage()
+	{
+		trigger_error("Not implemented");
+	}
+}

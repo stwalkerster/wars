b51423ae2482df08c791e9b009eddcc81f955814
Simon Walker <github@helpmebot.org.uk>
2011-04-16 17:40:35 +0100

    reset database script

---
+++ maintenance/devel/resetdb.php
@@ -0,0 +1,12 @@
+<?php
+define("WARS", 1);
+require_once 'config.inc.php';
+
+$olddatabase = "p_acc_test";
+
+require_once $baseMaintenancePath . 'devel/convert.inc';
+
+query("DROP TABLE $olddatabase.acc_log;");
+query("CREATE TABLE $olddatabase.acc_log LIKE $olddatabase.log_bu;");
+query("INSERT INTO $olddatabase.acc_log SELECT * FROM $olddatabase.log_bu;");
+out("done\n");

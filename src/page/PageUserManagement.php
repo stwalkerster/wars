1eb1e184f211d65757a27410038f4dfb1f87f645
Simon Walker <github@helpmebot.org.uk>
2011-05-01 18:27:22 +0100

    pass data to template

--- src/page/PageUserManagement.php
+++ src/page/PageUserManagement.php
@@ -23,6 +23,19 @@ class PageUserManagement extends PageBase
 
 	function runPage()
 	{
+		$users = array();
+		for ($i = 0; $i < 50; $i++) {
+			$u = User::getById($i);
+			if($u)
+			$users[]=$u;
+		}
+		
+		$this->smarty->assign('user_new', $users);
+		$this->smarty->assign('user_user', $users);
+		$this->smarty->assign('user_admin', $users);
+		$this->smarty->assign('user_checkuser', $users);
+		$this->smarty->assign('user_suspended', $users);
+		$this->smarty->assign('user_declined', $users);
 		$this->smarty->assign('subpage', 'page/UserManagement.tpl');
 	}
 }

491a22355e5d2930a5abc189720a98357e9bbc07
Simon Walker <github@helpmebot.org.uk>
2011-05-01 16:46:43 +0100

    create user management page

---
+++ src/page/PageUserManagement.php
@@ -0,0 +1,28 @@
+<?php
+/**************************************************************************
+*                     Wikipedia Account Request System                    *
+***************************************************************************
+*                                                                         *
+* Conceptualised by Incubez (author: X!) and ACC (author: SQL and others) *
+*                                                                         *
+* Please refer to /LICENCE for more info.                                 *
+*                                                                         *
+**************************************************************************/
+
+// check that this code is being called from a valid entry point. 
+if(!defined("WARS"))
+	die("Invalid code entry point!");
+
+class PageUserManagement extends PageBase
+{
+	
+	function __construct()
+	{
+		$this->subtitle = "User Management";
+	}
+
+	function runPage()
+	{
+		$this->smarty->assign('subpage', 'page/UserManagement.tpl');
+	}
+}
---
+++ template/page/UserManagement.tpl
@@ -0,0 +1,13 @@
+<p>This page allows you to manage the users of this system.</p>
+
+<h2>New Users</h2>
+
+<h2>Users</h2>
+
+<h2>Admins</h2>
+
+<h2>Checkusers</h2>
+
+<h2>Suspended Users</h2>
+
+<h2>Declined Users</h2>

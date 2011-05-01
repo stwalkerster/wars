44a840e47d38385e0bcf30e4e099b878747f8b70
Simon Walker <github@helpmebot.org.uk>
2011-05-01 02:57:19 +0100

    disable error handler for non-web clients

--- src/errorhandler.php
+++ src/errorhandler.php
@@ -114,14 +114,10 @@ function exception_handler(Exception $exception)
 	
 	$smarty->display('Page.tpl');
 }
-if($useErrorHandler)
+if($useErrorHandler && isset($_SERVER['REMOTE_ADDR']))
 {
 	set_error_handler("error_handler", E_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR | E_COMPILE_ERROR | E_CORE_ERROR );
 	set_exception_handler("exception_handler");
-	
-	// only enable output buffering if we have a remote client (ie: requested via http)
-	if(isset($_SERVER['REMOTE_ADDR']))
-	{
-		ob_start();
-	}
+
+	ob_start();
 }
